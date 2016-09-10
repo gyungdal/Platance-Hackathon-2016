0e72549964bd6e85f48ea645d07d7a8e2eb6ce1f
GyungDal <9274aa@gmail.com>
2016-09-10 17:39:31 +0900

    maybe... finish?

--- engine/getLocate/src/com/company/Main.java
+++ engine/getLocate/src/com/company/Main.java
@@ -2,9 +2,54 @@ package com.company;
 
 import com.company.locate.GetDistance;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        GetDistance getDistance = new GetDistance("서울특별시 강남구 도산대로 176", "서울 서초구 잠원동 58-16 신반포 10차 316-1204");
+        ArrayList<String> test = new ArrayList<>();
+        test.add("서울 강남구 논현동 122-8");
+        test.add("서울 서초구 잠원동 58-16 신반포 10차 316-1204");
+        test.add("서울특별시 강남구 도산대로 176");
+        test.add("서울 송파구 잠실동 19");
+        test.add("서울 송파구 잠실본동 320");
+        test.add("서울 강남구 대치동 511 한보미도맨션2차");
+        test.add("서울 강남구 도곡동 538");
+        test.add("서울 강남구 논현동 122-8");
+        int beforeDistance = 0, afterDistance = 0, temp = 0, temp2 = 0;
+        for(int i = 0;i<test.size()-1;i++)
+            beforeDistance += getValue(test.get(i), test.get(i+1));
+        ArrayList<String> clone, result;
+        clone = (ArrayList)test.clone();
+        result = (ArrayList)test.clone();
+        temp = beforeDistance;
+        for(int i = 1;i<clone.size() -1 ;i++){
+            for(int j = 1;j<clone.size() -1 ;j++){
+                temp2 = 0;
+                String TEMP = clone.get(i);
+                clone.set(i,clone.get(j));
+                clone.set(j,TEMP);
+                for(int k = 0;k<clone.size() - 1;k++)
+                    temp2 += getValue(clone.get(k), clone.get(k + 1));
+
+                System.out.println("TIME : " + temp2);
+                if(temp > temp2) {
+                    result = (ArrayList) clone.clone();
+                    temp = temp2;
+                }
+            }
+        }
+        afterDistance = temp;
+        System.out.println("ALL DISTANCE : " + beforeDistance);
+        System.out.println("AFTER DISTANCE : " + afterDistance);
+        for(String results : result)
+            System.out.println(results);
+       // System.out.println(g.getShortestPath(test.get(0), "서울 송파구 잠실본동 320"));
+    }
+
+    private static long getValue(String start, String end){
+        GetDistance getDistance = new GetDistance(start, end);
         getDistance.start();
+        while(getDistance.getResult() == -1);
+        return getDistance.getResult();
     }
 }
--- engine/getLocate/src/com/company/locate/GetDistance.java
+++ engine/getLocate/src/com/company/locate/GetDistance.java
@@ -1,11 +1,16 @@
 package com.company.locate;
 
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import sun.rmi.runtime.Log;
 
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -14,13 +19,21 @@ import java.net.URL;
 public class GetDistance extends Thread {
     private static final String GET_DISTANCE_REQUEST = "http://map.naver.com/spirra/findCarRoute.nhn?route=route3&output=json&coord_type=naver&search=0&car=0&mileage=12.4&";
     private static final String GET_LOCATE_REQUEST_FORMAT = "http://maps.google.com/maps/api/geocode/json?address=%s&sensor=false";
+    //private static final String GET_LOCATE_REQUEST_FORMAT = "https://apis.daum.net/local/geo/addr2coord?apikey=952d7bd3b757ddb384711627fbd29538&q=%s&output=json&appid=com.codezero.fireprevention";
     private String sp;
     private String ep;
-
+    private long result = -999;
     public GetDistance(String sp, String ep){
         this.sp = sp;
         this.ep = ep;
     }
+
+    public synchronized long getResult(){
+        if(result != -999)
+            return this.result;
+        return -1;
+    }
+
 
     @Override
     public void run(){
@@ -46,13 +59,16 @@ public class GetDistance extends Thread {
 
             JSONParser jsonParser = new JSONParser();
             JSONObject jsonObject = (JSONObject) jsonParser.parse(temp);
-            System.out.println(((JSONObject)jsonObject.get("summary")).get("totalDistance"));
+            System.out.println("Done");
+            result = Long.valueOf(((JSONObject)jsonObject.get("summary")).get("totalDistance").toString());
+            //System.out.println(result);
+
         }catch(Exception e){
             System.err.println(e.getMessage());
         }
     }
 
-    private Item getLocate(String name) throws Exception{
+    private String get(String name) throws IOException {
         System.out.println(String.format(GET_LOCATE_REQUEST_FORMAT, name).replaceAll(" ", "%20"));
         URL url = new URL(String.format(GET_LOCATE_REQUEST_FORMAT, name).replaceAll(" ", "%20"));
         url.openConnection();
@@ -68,17 +84,26 @@ public class GetDistance extends Thread {
             temp += inputLine;
         }
         in.close();
+        conn.disconnect();
+        return temp;
+    }
+    private Item getLocate(String name) throws Exception{
+        String temp = get(name);
         System.out.println(temp.toString());
+        while(temp.contains("OVER_QUERY_LIMIT")){
+            this.sleep(100);
+            temp = get(name);
+        }
         Item item = getPoint(temp);
         System.out.println(temp);
         System.out.println(item.lng);
         System.out.println(item.lat);
         return item;
     }
-    private Item getPoint(String temp){
+    private Item getPoint(String temp) throws InterruptedException {
         temp = temp.substring(temp.indexOf("\"lat\" :"), temp.length());
         temp = temp.substring(0, temp.indexOf("}")).trim();
         return new Item(Double.valueOf(temp.substring(temp.indexOf("\"lat\" : ") + "\"lat\" : ".length(), temp.indexOf(',')))
-            , Double.valueOf(temp.substring(temp.indexOf("\"lng\" : ") + "\"lat\" : ".length(), temp.length())));
+                , Double.valueOf(temp.substring(temp.indexOf("\"lng\" : ") + "\"lat\" : ".length(), temp.length())));
     }
 }
